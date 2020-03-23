2bb31ec94be80f34674c7bcbbdd11135e8d7b0db
BrujoCervino_Laptop <project.verna@gmail.com>
2020-03-23 00:53:11 +0000

    Intial (Manual) Commit

---
+++ src/com/Messarounds/Main.java
@@ -0,0 +1,17 @@
+package com.Messarounds;
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        super.run();
+    }
+}
+
+// Here I'm going to continue my threading tests:
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
